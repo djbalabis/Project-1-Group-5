592475548ced23e8b7d7140667af8f20db1f58fb
nipaanand <nipa.mehta@mail.com>
2022-09-27 19:29:06 -0400

    Map added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 //Google maps API
 
-<<<<<<< HEAD
-})
+/*})
 .then(function(data){
     console.log(data);
 });*/
-const options = {
+/*const options = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': '06c0694a15msh45ebf1c15bfb103p1b9c12jsn4b8372b0a32e',
@@ -14,24 +13,49 @@ const options = {
   };
   /*const apikey ="06c0694a15msh45ebf1c15bfb103p1b9c12jsn4b8372b0a32e"*/
   
-  fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/MI/city/West%20Bloomfield/0',options)
+  /*fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/MI/city/West%20Bloomfield/0',options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
-=======
-let map;
+*/
+// JavaScript Document
+var queryURL =
+  'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=nyc&category=restaurants';
+var apiKey = 'bFcEXqo4FCBDva9UcdbFDDtf3-9I9hmwKWoN4Anl3fNsHyfZOcD8hypZ8HSDTLMwk2vQYgW8eOTV23_IcCx30o5pNfobQoQLxurs67zM7CfYv-sIxHsobpBsBnAzY3Yx';
+
+$.ajax({
+  url: queryURL,
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    'x-requested-with': 'xmlhttprequest',
+    'Access-Control-Allow-Origin': '*',
+    Authorization: `Bearer OEfT0vwGcTKa-Vf4ZkPXf4Jax8psNp-064XV7CFYGxfv6tBYWsQgL52HmCoHoFtDNNiIFDdobq5Yvhw6dnYsxkGcmL-axC2eIixeSgz-CWg7UKvKefy7ByEOYToyY3Yx`,
+    
+  },
+  success: function (result) {
+    console.log(result);
+  },
+});
+
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+  
+ //let  map = new google.maps.Map(document.getElementById("map"),
+ var options= {
+    center: { lat: 40.5187, lng: 74.4121 },
     zoom: 8,
-  });
-}
+  };
+  
 
+
+  let map =new google.maps.Map(document.getElementById('map'),options);
+
+}
 window.initMap = initMap;
 
 // JavaScript Document
-var queryURL =
+/*var queryURL =
   "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=nyc&category=restaurants";
 var apiKey = "my key";
 
@@ -47,5 +71,4 @@ $.ajax({
   success: function (result) {
     console.log(result);
   },
-});
->>>>>>> 99fc763641a3c1ac0b1bf6116af9a1c2fc9da1fe
+});*/
